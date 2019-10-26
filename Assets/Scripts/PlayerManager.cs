d6e657eda067e70b5a816cf97c4b4133221c0ee1
Xwilarg <xwilarg@yahoo.fr>
2019-10-27 00:15:45 +0900

    Improve starting flow

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] spawns;
+
+    private ControllerDetection controller;
+
+    private void Start()
+    {
+        controller = GameObject.Find("IntroController").GetComponent<ControllerDetection>();
+    }
+}
