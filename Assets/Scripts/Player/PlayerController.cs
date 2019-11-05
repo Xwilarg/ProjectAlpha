6838567ffe97ab797525df0b0e23667507842eab
Xwilarg <xwilarg@yahoo.fr>
2019-11-05 13:07:34 +0900

    Change scripts to use new Input System

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,30 +5,34 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private User user;
-
     private Rigidbody rb;
-
-    private const float speed = 10f;
-
     private PlayerInput input;
+    private const float speed = 10f;
+    private Vector2 mov;
 
     public void SetUser(User value)
     {
+        user = value;
+        input = GetComponent<PlayerInput>();
         if (user.GetControllerId() == -1)
             input.defaultControlScheme = "KeyboardMouse";
         else
             input.defaultControlScheme = "Gamepad";
-        user = value;
     }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        input = GetComponent<PlayerInput>();
+        mov = Vector2.zero;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = user.GetMovement() * speed;
+        rb.velocity = new Vector3(mov.x, rb.velocity.y, mov.y) * speed;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        mov = value.Get<Vector2>();
     }
 }
--- Assets/Scripts/Player/User.cs
+++ Assets/Scripts/Player/User.cs
@@ -1,62 +1,13 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class User
 {
-    public static readonly int button0KeyCode = (int)KeyCode.Joystick1Button0;
-    private static readonly int nextJoystick = 20; // That means that if you add 20 to button0KeyCode you get the button 0 of the joystick 2
-
-    public static Dictionary<string, Key> keybind = new Dictionary<string, Key>()
-    {
-        { "left",   new Key{ keyboard = KeyCode.A, keycontroller = (KeyCode)(button0KeyCode + 14) } },
-        { "right",  new Key{ keyboard = KeyCode.D, keycontroller = (KeyCode)(button0KeyCode + 15) } },
-        { "up",     new Key{ keyboard = KeyCode.W, keycontroller = (KeyCode)(button0KeyCode + 12) } },
-        { "down",   new Key{ keyboard = KeyCode.S, keycontroller = (KeyCode)(button0KeyCode + 13) } }
-    };
-
-    public struct Key
-    {
-        public KeyCode keyboard;
-        public KeyCode keycontroller;
-    }
-
     public User(int controller, Text controllerText)
     {
         _controller = controller;
         string name = _controller == -1 ? "Keyboard" : Input.GetJoystickNames()[_controller];
         controllerText.text = name;
-    }
-
-    public bool GetKeyDown(string key)
-        => Input.GetKeyDown(GetKeyInternal(key));
-
-    public bool GetKey(string key)
-        => Input.GetKey(GetKeyInternal(key));
-
-    private KeyCode GetKeyInternal(string key)
-        => _controller == -1 ? keybind[key].keyboard : (KeyCode)((int)(keybind[key].keycontroller) + (nextJoystick * _controller));
-
-    public Vector3 GetMovement()
-    {
-        float x = 0f;
-        float y = 0f;
-
-        if (_controller > -1f)
-        {
-            x = Input.GetAxis("Joy" + _controller + "X");
-            y = Input.GetAxis("Joy" + _controller + "Y");
-        }
-        if (GetKey("left"))
-            x = -1f;
-        else if (GetKey("right"))
-            x = 1f;
-        if (GetKey("up"))
-            y = 1f;
-        else if (GetKey("down"))
-            y = -1f;
-
-        return new Vector3(x, 0f, y);
     }
 
     public int GetControllerId()
