a23a9163791c43412f41e106e19ed12f70582750
Xwilarg <xwilarg@yahoo.fr>
2019-11-09 18:11:48 +0900

    Fix a bug where game could start without any player

--- Assets/Scripts/Menu/StartToInvoke.cs
+++ Assets/Scripts/Menu/StartToInvoke.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class StartToInvoke : MonoBehaviour
 {
+    private Button b;
+
+    private void Start()
+    {
+        b = GetComponent<Button>();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button7) ||
+        if (b.interactable && (Input.GetKeyDown(KeyCode.Joystick1Button7) ||
             Input.GetKeyDown(KeyCode.Joystick2Button7) ||
             Input.GetKeyDown(KeyCode.Joystick3Button7) ||
-            Input.GetKeyDown(KeyCode.Joystick4Button7))
-            GetComponent<Button>().onClick.Invoke();
+            Input.GetKeyDown(KeyCode.Joystick4Button7)))
+            b.onClick.Invoke();
     }
 }
