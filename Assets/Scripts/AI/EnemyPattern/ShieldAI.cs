3e9a1a5062d464cfc5cf2762fbf421cc874f2813
Xwilarg <xwilarg@yahoo.fr>
2019-11-14 13:56:02 +0900

    Shield AI now keep looking at player

--- Assets/Scripts/AI/EnemyPattern/ShieldAI.cs
+++ Assets/Scripts/AI/EnemyPattern/ShieldAI.cs
@@ -3,15 +3,18 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(EnemyAI))]
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(PlayerController))]
 public class ShieldAI : MonoBehaviour
 {
     private NavMeshAgent agent;
     private EnemyAI ai;
+    private PlayerController pc;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         ai = GetComponent<EnemyAI>();
+        pc = GetComponent<PlayerController>();
     }
 
     private void Update()
@@ -19,7 +22,11 @@ public class ShieldAI : MonoBehaviour
         float distance;
         var closest = AIUtilities.GetClosestTransform(transform.position, ai.Humans, out distance).position;
         if (distance < 10f)
+        {
             agent.SetDestination(transform.position);
+            var angle = Mathf.Atan2(closest.z - transform.position.z, closest.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+            pc.SetRotation(Quaternion.Euler(0f, -angle, 0f));
+        }
         else
             agent.SetDestination(closest);
     }
