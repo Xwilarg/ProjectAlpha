8b55fb2eb83aa333f40d6c6761ba5a7a301fc697
Xwilarg <xwilarg@yahoo.fr>
2019-11-18 11:35:55 +0900

    Add shield protection for explosions

--- Assets/Scripts/Weapon/Bullet/Grenade.cs
+++ Assets/Scripts/Weapon/Bullet/Grenade.cs
@@ -5,6 +5,7 @@ public class Grenade : ABullet
     private Shake camShake;
     private const float shakeForce = 1f; // Force of the screen shake
     private const float shakeDuration = .1f;
+    private const float grenadeRadius = 2f;
 
     private void Start()
     {
@@ -13,10 +14,15 @@ public class Grenade : ABullet
 
     protected override void OnCollision(Collision collision)
     {
-        var enemy = collision.collider.GetComponent<EnemyAI>();
-        if (enemy != null)
+        Vector3 explosionPos = collision.GetContact(0).point;
+        foreach (var collider in Physics.OverlapSphere(explosionPos, grenadeRadius, 1))
         {
-            enemy.LooseHp(1);
+            var enemy = collider.GetComponent<EnemyAI>();
+            if (enemy != null)
+            {
+                if (Physics.Linecast(transform.position, explosionPos, ~(1 << 9))) // Is the enemy protected by a shield?
+                    enemy.LooseHp(5);
+            }
         }
         camShake.ShakeMe(shakeForce, shakeDuration);
     }
